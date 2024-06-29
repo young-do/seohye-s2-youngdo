22323bbb3b276aaecedd07eac3602e9fbfbfa2ba
young-do <yd199323@gmail.com>
2024-06-30 01:38:11 +0900

    firebase 데이터 쓰기 제거

--- src/components/Cheerup.svelte
+++ src/components/Cheerup.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, onValue, update, increment } from 'firebase/database';
+import { getDatabase, ref, get } from 'firebase/database';
 import { onMount } from 'svelte';
 import { format } from 'date-fns';
 import Button_Heart from './Button_Heart.svelte';
@@ -23,30 +23,13 @@ const app = initializeApp(firebaseConfig);
 getAnalytics(app);
 
 const db = getDatabase(app);
-const heartsRef = ref(db, 'hearts');
 const messagesRef = ref(db, 'messages');
 
 type MessageRawObject = { [datetime: string]: { name: string; text: string } };
 type Message = { id: string; name: string; text: string; datetime: string };
-let heartCount = 0;
+let heartCount = 220327;
 let messages: Message[] = [];
-let typedNickname = '';
-let typedMessage = '';
-$: enabledSendButton = typedNickname.length > 0 && typedMessage.length > 0;
 
-const onClickHeart = () => {
-  update(heartsRef, { count: increment(1) });
-};
-const onSubmit = () => {
-  const name = typedNickname.trim();
-  const text = typedMessage.trim();
-  if (name.length === 0 || text.length === 0) {
-    alert('빈 공백은 안됩니다. :(');
-  }
-  const datetime = Date.now().toString();
-  update(messagesRef, { [datetime]: { name, text } });
-  typedMessage = '';
-};
 // 내림차순 정렬
 const sortString = (a: string, b: string) => {
   if (a > b) return -1;
@@ -55,11 +38,7 @@ const sortString = (a: string, b: string) => {
 };
 
 onMount(() => {
-  onValue(heartsRef, snapshot => {
-    const value = snapshot.val() ?? { count: 0 };
-    heartCount = value.count;
-  });
-  onValue(messagesRef, snapshot => {
+  get(messagesRef).then(snapshot => {
     const value: MessageRawObject = snapshot.val() ?? {};
     messages = Object.entries(value)
       .map(([id, value]) => {
@@ -76,18 +55,11 @@ onMount(() => {
 
   <div>
     조영도
-    <Button_Heart count={heartCount} onClick={onClickHeart} />
+    <Button_Heart count={heartCount} />
     김서혜
   </div>
 
   <div class="message-container">
-    <form on:submit|preventDefault={onSubmit}>
-      <textarea class="message-form-text" bind:value={typedMessage} placeholder="축하글을 남겨주세요." />
-      <div class="message-form-subbox">
-        <input class="message-form-input" type="text" bind:value={typedNickname} placeholder="이름 입력" />
-        <button class="message-form-button" type="submit" disabled={!enabledSendButton}>등록하기</button>
-      </div>
-    </form>
     <ul class="message-list">
       {#each messages as message (message.id)}
         <li class="message-item">
@@ -125,32 +97,6 @@ h2 {
   margin: 0 auto;
   padding: 0 1.5rem;
   font-size: 0.875rem;
-}
-.message-form-text {
-  display: block;
-  width: 100%;
-  height: 5rem;
-  padding: 0.5rem;
-  margin-bottom: 0.5rem;
-  border: 1px solid #c4c4c4;
-}
-.message-form-subbox {
-  display: flex;
-  flex-flow: row;
-}
-.message-form-input {
-  flex: 1;
-  width: 100%;
-  padding: 0.5rem;
-  margin-right: 0.5rem;
-  border: 1px solid #c4c4c4;
-}
-.message-form-button {
-  padding: 0.5rem 1rem;
-  background: #f65c8a;
-  color: #fff;
-  font-weight: bold;
-  flex-shrink: 0;
 }
 .message-list {
   min-height: 300px;
